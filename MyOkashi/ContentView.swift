820a6d46bc79aed2c2261a2f517a8253c28d06f7
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-06-23 16:16:40 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by administrator on 2021/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by administrator on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
